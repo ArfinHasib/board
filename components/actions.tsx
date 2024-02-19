2a3a5087ea4421bc9f6739d1065690a157aa2814
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-19 16:37:28 +0600

    Card Actions (Finalize Rename, Delete, Copy Link)

--- components/actions.tsx
+++ components/actions.tsx
@@ -11,11 +11,12 @@ import {
    DropdownMenuItem,
    DropdownMenuSeparator,
 } from './ui/dropdown-menu';
-import { Link2, Trash2 } from 'lucide-react';
+import { Link2, Pencil, Trash2 } from 'lucide-react';
 import { toast } from 'sonner';
 import { useApiMutation } from '@/hooks/use-api-mutation';
 import { api } from '@/convex/_generated/api';
 import { Button } from './ui/button';
+import { useRenameModal } from '@/store/user-rename-modal';
 
 interface ActionProps {
    children: React.ReactNode;
@@ -32,6 +33,7 @@ export const Actions = ({
    id,
    title,
 }: ActionProps) => {
+   const { onOpen } = useRenameModal();
    const { mutate, pending } = useApiMutation(api.board.remove);
 
    const onCopyLink = () => {
@@ -62,6 +64,13 @@ export const Actions = ({
             >
                <Link2 className='h-4 w-4 mr-2' />
                Copy board link
+            </DropdownMenuItem>
+            <DropdownMenuItem
+               onClick={() => onOpen(id, title)}
+               className='p-3 cursor-pointer'
+            >
+               <Pencil className='h-4 w-4 mr-2' />
+               Rename
             </DropdownMenuItem>
             <ConfirmModal
                header='Delete Board'
--- store/user-rename-modal.ts
+++ store/user-rename-modal.ts
@@ -2,19 +2,19 @@ import { create } from 'zustand';
 
 const defaultValues = { id: '', title: '' };
 
-interface IrenameModal {
+interface IRenameModal {
    isOpen: boolean;
    initialValues: typeof defaultValues;
    onOpen: (id: string, title: string) => void;
    onClose: () => void;
 }
 
-export const useRenameModal = create<IrenameModal>((set) => ({
+export const useRenameModal = create<IRenameModal>((set) => ({
    isOpen: false,
    onOpen: (id, title) =>
       set({
-         isOpen: false,
-         initialValues: defaultValues,
+         isOpen: true,
+         initialValues: { id, title },
       }),
    onClose: () =>
       set({
