5f536e0b65212750683499503d876ed97141b994
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-19 14:07:27 +0600

    Delete Modal and confirm component

--- components/actions.tsx
+++ components/actions.tsx
@@ -2,6 +2,8 @@
 
 import { DropdownMenuContentProps } from '@radix-ui/react-dropdown-menu';
 
+import { ConfirmModal } from './confirm-modal';
+
 import {
    DropdownMenu,
    DropdownMenuTrigger,
@@ -13,6 +15,7 @@ import { Link2, Trash2 } from 'lucide-react';
 import { toast } from 'sonner';
 import { useApiMutation } from '@/hooks/use-api-mutation';
 import { api } from '@/convex/_generated/api';
+import { Button } from './ui/button';
 
 interface ActionProps {
    children: React.ReactNode;
@@ -60,11 +63,20 @@ export const Actions = ({
                <Link2 className='h-4 w-4 mr-2' />
                Copy board link
             </DropdownMenuItem>
-
-            <DropdownMenuItem onClick={onDelete} className='p-3 cursor-pointer'>
-               <Trash2 className='h-4 w-4 mr-2' />
-               Delete
-            </DropdownMenuItem>
+            <ConfirmModal
+               header='Delete Board'
+               description='This will delete the board and all of its contents.'
+               disabled={pending}
+               onConfirm={onDelete}
+            >
+               <Button
+                  variant='ghost'
+                  className='p-3 cursor-pointer text-sm w-full justify-start font-normal'
+               >
+                  <Trash2 className='h-4 w-4 mr-2' />
+                  Delete
+               </Button>
+            </ConfirmModal>
          </DropdownMenuContent>
       </DropdownMenu>
    );
