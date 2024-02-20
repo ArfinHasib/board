af79fcfd73b155e4110cb3093482928eb49117c1
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-20 17:16:45 +0600

    LiveBlocks Setup

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,10 +1,18 @@
 import { Canvas } from './_components/canvas';
+import { Room } from '@/components/room';
+import { Loading } from './_components/loading';
 
-const BoardIdPage = () => {
+interface BoardIdPageProps {
+   params: {
+      boardId: string;
+   };
+}
+
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
    return (
-      <div>
-         <Canvas />
-      </div>
+      <Room roomId={params.boardId} fallback={<Loading />}>
+         <Canvas boardId={params.boardId} />
+      </Room>
    );
 };
 
