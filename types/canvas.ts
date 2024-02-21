b7ddd4390d66db18a310cb587b31c419c540e912
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-21 18:39:26 +0600

    Canvas State

---
+++ types/canvas.ts
@@ -0,0 +1,134 @@
+export type Color = {
+   r: number;
+   g: number;
+   b: number;
+};
+
+export type Camera = {
+   x: number;
+   y: number;
+};
+
+export enum LayerType {
+   Rectangle,
+   Elipse,
+   Path,
+   Text,
+   Note,
+}
+
+export type RectangleLayer = {
+   type: LayerType.Rectangle;
+   x: number;
+   y: number;
+   height: number;
+   width: number;
+   fill: Color;
+   value?: number;
+};
+
+export type ElipseLayer = {
+   type: LayerType.Elipse;
+   x: number;
+   y: number;
+   height: number;
+   width: number;
+   fill: Color;
+   value?: number;
+};
+
+export type PathLayer = {
+   type: LayerType.Path;
+   x: number;
+   y: number;
+   height: number;
+   width: number;
+   fill: Color;
+   points: [][];
+   value?: number;
+};
+
+export type TextLayer = {
+   type: LayerType.Text;
+   x: number;
+   y: number;
+   height: number;
+   width: number;
+   fill: Color;
+   value?: number;
+};
+
+export type NoteLayer = {
+   type: LayerType.Note;
+   x: number;
+   y: number;
+   height: number;
+   width: number;
+   fill: Color;
+   value?: number;
+};
+
+export type Point = {
+   x: number;
+   y: number;
+};
+
+export type XYWH = {
+   x: number;
+   y: number;
+   width: number;
+   height: number;
+};
+
+export enum Side {
+   Top = 1,
+   Bottom = 2,
+   Left = 4,
+   Right = 8,
+}
+
+export type CanvasState =
+   | {
+        mode: CanvasMode.None;
+     }
+   | {
+        mode: CanvasMode.SelectionNet;
+        origin: Point;
+        current?: Point;
+     }
+   | {
+        mode: CanvasMode.Translating;
+     }
+   | {
+        mode: CanvasMode.Inserting;
+        layerType:
+           | LayerType.Elipse
+           | LayerType.Rectangle
+           | LayerType.Note
+           | LayerType.Text;
+     }
+   | {
+        mode: CanvasMode.Pencil;
+     }
+   | {
+        mode: CanvasMode.Pressing;
+        origin: Point;
+     }
+   | {
+        mode: CanvasMode.Resizing;
+        initalBounds: XYWH;
+        corner: Side;
+     }
+   | {
+        mode: CanvasMode.Resizing;
+     };
+
+export enum CanvasMode {
+   None,
+   Pressing,
+   SelectionNet,
+   Translating,
+   Inserting,
+   Resizing,
+   Pencil,
+}
