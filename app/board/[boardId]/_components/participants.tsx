27f2f6c468ea5e18d3d6ed2f9bc7fa684c9c618a
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-22 16:09:55 +0600

    Cursors Presence

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,21 +1,21 @@
 'use client';
 
+import { connectionIdToColor } from '@/lib/utils';
 import { useOthers, useSelf } from '@/liveblocks.config';
 
 import { UserAvatar } from './user-avatar';
-import { connectionIdToColor } from '@/lib/utils';
 
-const MAX_SHOWN_USERs = 3;
+const MAX_SHOWN_USERS = 2;
 
 export const Participants = () => {
    const users = useOthers();
    const currentUser = useSelf();
-   const hasMoreUsers = users.length > MAX_SHOWN_USERs;
+   const hasMoreUsers = users.length > MAX_SHOWN_USERS;
 
    return (
       <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md'>
          <div className='flex gap-x-2'>
-            {users.slice(0, MAX_SHOWN_USERs).map(({ connectionId, info }) => {
+            {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
                return (
                   <UserAvatar
                      borderColor={connectionIdToColor(connectionId)}
@@ -30,7 +30,6 @@ export const Participants = () => {
             {currentUser && (
                <UserAvatar
                   borderColor={connectionIdToColor(currentUser.connectionId)}
-                  key={currentUser.connectionId}
                   src={currentUser.info?.picture}
                   name={`${currentUser.info?.name} (You)`}
                   fallback={currentUser.info?.name?.[0]}
@@ -39,8 +38,8 @@ export const Participants = () => {
 
             {hasMoreUsers && (
                <UserAvatar
-                  name={`${users.length - MAX_SHOWN_USERs} more`}
-                  fallback={`+${users.length - MAX_SHOWN_USERs}`}
+                  name={`${users.length - MAX_SHOWN_USERS} more`}
+                  fallback={`+${users.length - MAX_SHOWN_USERS}`}
                />
             )}
          </div>
@@ -50,6 +49,6 @@ export const Participants = () => {
 
 export const ParticipantsSkeleton = () => {
    return (
-      <div className='absolute top-2 right-2 bg-white rounded-md p-3 h-12 flex items-center shadow-md w-[100px]' />
+      <div className='absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]' />
    );
 };
