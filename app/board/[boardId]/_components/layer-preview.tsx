098af951946972db54ebed40cbf7e20e518d5bad
ArfinHasib <arfinhasib21375@gmail.com>
2024-02-26 12:54:40 +0600

    Other Components (text)

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -7,6 +7,7 @@ import { useStorage } from '@/liveblocks.config';
 
 import { Rectangle } from './rectangle';
 import { Ellipse } from './ellipse';
+import { Text } from './text';
 
 interface LayerPreviewProps {
    id: string;
@@ -26,6 +27,16 @@ export const LayerPreview = memo(
          case LayerType.Rectangle:
             return (
                <Rectangle
+                  id={id}
+                  layer={layer}
+                  onPointerDown={onLayerPointerDown}
+                  selectionColor={selectionColor}
+               />
+            );
+
+         case LayerType.Text:
+            return (
+               <Text
                   id={id}
                   layer={layer}
                   onPointerDown={onLayerPointerDown}
